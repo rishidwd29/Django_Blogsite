220ceb79a7b76173324cab9fafe7467b336b126a
rishidwd29 <rishidwd29@gmail.com>
2022-11-08 22:20:43 +0530

    added bootstrape and base.html to the template

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+posts = [
+    {
+        'author': 'John',
+        'title': 'blog post 1',
+        'content': 'First post content',
+        'date_posted': 'August 27, 2018'
+    },
+    {
+        'author': 'John doe',
+        'title': 'blog post 2',
+        'content': 'second post content',
+        'date_posted': 'August 28, 2018'
+    },
+]
+
 def home(request):
-    return render(request, "blog/home.html")
+    context = {
+        'posts':posts
+    }
+    return render(request, "blog/home.html",context)
 def About(request):
-    return HttpResponse("<B><h1>Blog About</h1></B>")
+    return render(request,"blog/about.html",{'title':'about'})
 
